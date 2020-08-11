3826d301f28f84efaccfe669ed7c5505596ef079
Mike Hayes <mikehayes330@gmail.com>
2020-08-10 23:32:33 -0400

    changed route to /

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ let data = JSON.parse(dataDB);
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 8080;
+var PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -24,9 +24,9 @@ app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "/public/index.html"));
-// });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 app.get("/api/notes", function (req, res) {
   return res.json(data);
